81d92b894c6c8be8a3d606435793c219d4d6362a
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2026-04-04 16:14:18 +0900

    chore: set spring boot project

---
+++ src/main/kotlin/depromeet/hotsix/obrit/ObritApplication.kt
@@ -0,0 +1,11 @@
+package depromeet.hotsix.obrit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ObritApplication
+
+fun main(args: Array<String>) {
+    runApplication<ObritApplication>(*args)
+}
---
+++ src/test/kotlin/depromeet/hotsix/obrit/ObritApplicationTests.kt
@@ -0,0 +1,13 @@
+package depromeet.hotsix.obrit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ObritApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
