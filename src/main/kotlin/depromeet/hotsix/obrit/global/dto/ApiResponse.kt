80958fc523aec41f2a580ec9017c05221f9db4f4
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2026-05-06 13:37:42 +0900

    chore(dto): ApiResponse 팩토리 메서드 추가

--- src/main/kotlin/depromeet/hotsix/obrit/global/dto/ApiResponse.kt
+++ src/main/kotlin/depromeet/hotsix/obrit/global/dto/ApiResponse.kt
@@ -1,3 +1,14 @@
 package depromeet.hotsix.obrit.global.dto
 
-data class ApiResponse<T>(val success: Boolean, val data: T? = null, val message: String? = null)
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class ApiResponse<T>(val success: Boolean, val message: String? = null, val data: T? = null) {
+    companion object {
+        fun <T> ok(data: T) = ApiResponse(true, null, data)
+
+        fun <T> ok(message: String, data: T) = ApiResponse(true, message, data)
+
+        fun <T> fail(message: String) = ApiResponse<T>(false, message, null)
+    }
+}
