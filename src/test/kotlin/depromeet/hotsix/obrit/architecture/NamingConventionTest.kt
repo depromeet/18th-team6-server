5b3268e19bd261650fe4858bdaba16088543549f
eclipse021 <yahang2008@naver.com>
2026-05-10 02:47:25 +0900

    test: 하네스 네이밍 규칙 보정

--- src/test/kotlin/depromeet/hotsix/obrit/architecture/NamingConventionTest.kt
+++ src/test/kotlin/depromeet/hotsix/obrit/architecture/NamingConventionTest.kt
@@ -39,6 +39,7 @@ class NamingConventionTest {
     fun `controller 패키지의 클래스는 Controller로 끝나야 한다`() {
         classes()
             .that().resideInAPackage("..controller..")
+            .and().areTopLevelClasses()
             .should().haveSimpleNameEndingWith("Controller")
             .because("controller 패키지의 클래스는 *Controller 네이밍을 따라야 합니다.")
             .allowEmptyShould(true)
@@ -49,6 +50,7 @@ class NamingConventionTest {
     fun `service 패키지의 클래스는 Service로 끝나야 한다`() {
         classes()
             .that().resideInAPackage("..service..")
+            .and().areTopLevelClasses()
             .should().haveSimpleNameEndingWith("Service")
             .because("service 패키지의 클래스는 *Service 네이밍을 따라야 합니다.")
             .allowEmptyShould(true)
@@ -59,6 +61,7 @@ class NamingConventionTest {
     fun `repository 패키지의 클래스는 Repository로 끝나야 한다`() {
         classes()
             .that().resideInAPackage("..repository..")
+            .and().areTopLevelClasses()
             .should().haveSimpleNameEndingWith("Repository")
             .orShould().haveSimpleNameEndingWith("RepositoryImpl")
             .because("repository 패키지의 클래스는 *Repository 또는 *RepositoryImpl 네이밍을 따라야 합니다.")
